df622446bbc8c1af06e6323023afdd0469c38bcb
DSLZL <q2891362919@163.com>
2024-12-27 23:31:37 +0800

    test

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/27/hello-world/"];function toRandomPost(){
+var posts=["2024/12/27/2/","2024/12/27/1/","2024/12/27/5/","2024/12/27/4/","2024/12/27/临时公告/","2024/12/27/3/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
