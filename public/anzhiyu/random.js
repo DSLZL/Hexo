b2c4f921f3116544fdff72be593d3c8d5fddfc44
DSLZL <q2891362919@163.com>
2025-01-03 16:15:47 +0800

    更新配置文件

--- public/anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/12/27/2/","2024/12/27/1/","2024/12/27/5/","2024/12/27/4/","2024/12/27/临时公告/","2024/12/27/3/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
